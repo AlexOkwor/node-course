ca62b8bb6b11906fe2effc4c1840e8795c1d9999
Alex <alexogokwor@yahoo.com>
2023-01-02 13:52:05 +0100

    Configure ssh keys with github and create access token on github

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ app.get('/', (req,res)=>{
 })
 
 app.listen(3000, ()=>console.log('Listening on port 3000...'));
+
+// I had to generate personal access token for my github account. 
+// When I gave the git push command, I encountered an error of access denied, 
+// since password authentication was no longer used.
+// I  configured ssh keys with github
+// I created access token on git hub
+// To set up git hub use:
+// git config --global user.name "Alex"
+// git config --global user.email alexogokwor@yahoo.com
